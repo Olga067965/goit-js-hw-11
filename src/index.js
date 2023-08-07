706726ad94e6a66b26169697062de1878fb5f1e7
Olga067965 <olgasavelieva5@gmail.com>
2023-08-07 22:05:20 +0300

    Update5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { scrollTo } from 'scroll-polyfill';
 
 export const fetchImages = async (inputValue, pageNumber) => {
   return await fetch(
@@ -28,58 +30,74 @@ const loadMoreButton = document.querySelector('.load-more');
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
 let pageNumber = 1;
+let totalPages = null;
 
-loadMoreButton.classList.add('d-none'); 
+loadMoreButton.classList.add('d-none');
 
-searchButton.addEventListener('click', e => {
+searchButton.addEventListener('click', async e => {
   e.preventDefault();
   clearGallery();
+
   const trimmedValue = input.value.trim();
   if (trimmedValue !== '') {
-    fetchImages(trimmedValue, pageNumber).then(foundData => {
-      if (foundData.hits.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        renderImgLst(foundData.hits);
-        Notiflix.Notify.success(
-          `Hooray! We found ${foundData.totalHits} images.`
-        );
-        loadMoreButton.classList.add('d-block'); 
-        gallerySimpleLightbox.refresh();
-      }
-    });
+    try {
+      const foundData = await fetchImages(trimmedValue, pageNumber);
+      handleSearchResponse(foundData);
+    } catch (error) {
+      console.error(error);
+    }
   } else {
     Notiflix.Notify.failure('Please enter a search query.');
     loadMoreButton.classList.add('d-none');
   }
 });
 
-loadMoreButton.addEventListener('click', () => {
+loadMoreButton.addEventListener('click', async () => {
   pageNumber++;
   const trimmedValue = input.value.trim();
-  loadMoreButton.classList.remove('d-none'); 
-  fetchImages(trimmedValue, pageNumber).then(foundData => {
-    if (foundData.hits.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
-      renderImgLst(foundData.hits);
-      Notiflix.Notify.success(
-        `Hooray! We found ${foundData.totalHits} images.`
-      );
-      loadMoreButton.classList.add('d-block'); 
-    }
-  });
+  loadMoreButton.classList.remove('d-none');
+
+  try {
+    const foundData = await fetchImages(trimmedValue, pageNumber);
+    handleSearchResponse(foundData);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
+function handleSearchResponse(foundData) {
+  if (foundData.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  if (pageNumber === 1) {
+    Notiflix.Notify.success(`Hooray! We found ${foundData.totalHits} images.`);
+  }
+
+  totalPages = Math.ceil(foundData.totalHits / 40);
+
+  if (pageNumber >= totalPages) {
+    loadMoreButton.classList.add('d-none');
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+
+  renderImgLst(foundData.hits);
+
+  if (pageNumber > 1) {
+    scrollTheCollection();
+  }
+
+  gallerySimpleLightbox.refresh();
+}
+
 function renderImgLst(images) {
-  console.log(images, 'images');
   const markup = images
     .map(image => {
-      console.log('img', image);
       return `<div class="photo-card">
        <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
         <div class="info">
@@ -99,10 +117,23 @@ function renderImgLst(images) {
     </div>`;
     })
     .join('');
-  gallery.innerHTML += markup;
+
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 function clearGallery() {
   gallery.innerHTML = '';
   pageNumber = 1;
-  loadMoreButton.classList.remove('d-none'); 
+  loadMoreButton.classList.remove('d-none');
+}
+
+function scrollTheCollection() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  scrollTo({
+    top: window.pageYOffset + cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
